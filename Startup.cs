5fd1aab790d87000c24d16ff25bbaf94ba1e3aa3
Prashant <prashant.niroula67@gmail.com>
2020-10-07 14:56:02 +0545

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bagmati.Startup))]
+namespace Bagmati
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
